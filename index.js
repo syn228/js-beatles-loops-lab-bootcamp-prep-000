931dcae6997b808ef8db03cceefee6349ad25294
Aaron Na <syn228@nyu.edu>
2018-03-28 16:49:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay() {
+  const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var array = [];
+  for (let i = 0; i < 4; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+function johnLennonFacts() {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  while (facts.length < 5) {
+    console.log(`${facts}!!!`)
+  }
+  return facts
+}
